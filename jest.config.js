bdc180b902121b28aa973af8a40a2ee854ec0f6d
MartinCesano <cesano305@gmail.com>
2025-06-28 19:30:40 +0000

    Se incorpora la libreria jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const { createDefaultPreset } = require("ts-jest");
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "ts"
+  ],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  collectCoverageFrom: [
+    "**/*.(t|j)s"
+  ],
+  coverageDirectory: "../coverage"
+};
